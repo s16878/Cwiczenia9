7c8abdedfd787f943011d1203f3e459b85b2e4fd
Jakub Ciećwierz <s16878@pjwstk.edu.pl>
2020-05-05 19:58:17 +0200

    Dodaj pliki projektów.

---
+++ LinqConsoleApp/Emp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqConsoleApp
+{
+    public class Emp
+    {
+        public int Empno { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+        public DateTime? HireDate { get; set; }
+        public int? Deptno { get; set; }
+        public Emp Mgr { get; set; }
+        public object Orders { get; internal set; }
+
+        public override string ToString()
+        {
+            return Ename + " (" + Empno + ")";
+        }
+    }
+}
